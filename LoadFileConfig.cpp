eb985c70d2f3bf00c26ef5ce1c825326b0a4c91a
Qwery <107665872+Qwery014@users.noreply.github.com>
2024-03-14 14:22:34 +0600

    finish

---
+++ LoadFileConfig.cpp
@@ -0,0 +1,31 @@
+#include "CMyWindow.h"
+
+/*
+void CMyWindow::
+LoadFileConfigFunc(
+    int width,
+    int height,
+    int imgWidth,
+    int imgHeight,
+    string url,
+    int rows,
+    int columns,
+    int lastRow,
+    int l,
+    int t,
+    int id
+)
+{
+	LoadFileConfigVar.width = width;
+	LoadFileConfigVar.height = height;
+	LoadFileConfigVar.url = url;
+	LoadFileConfigVar.imgWidth = imgWidth;
+	LoadFileConfigVar.imgHeight = imgHeight;
+	LoadFileConfigVar.rows = rows;
+	LoadFileConfigVar.columns = columns;
+	LoadFileConfigVar.lastRow = lastRow;
+    LoadFileConfigVar.l = l;
+    LoadFileConfigVar.t = t;
+
+    LoadFile();
+}*/
